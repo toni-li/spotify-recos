8f7c49231dd6a0492e21fa97e5e03b4f985b6b92
toni-li <toni.li@uwaterloo.ca>
2021-03-13 11:54:51 -0500

    fixing indent

--- app/main.py
+++ app/main.py
@@ -14,85 +14,85 @@ app = Flask(__name__)
 
 def process(user_id, song):
   import requests
-    #import sys
-    import spotipy
-    import spotipy.util as util
+  import sys
+  import spotipy
+  import spotipy.util as util
 
-    scope = 'playlist-read-private, playlist-read-collaborative'
+  scope = 'playlist-read-private, playlist-read-collaborative'
 
-    util.prompt_for_user_token("toni1li131",scope,client_id='4300c682d48b480d96478da07107ca59',client_secret='5a034c056898492b966e34c01a995d28',redirect_uri='https://oauth.pstmn.io/v1/browser-callback')
+  util.prompt_for_user_token("toni1li131",scope,client_id='4300c682d48b480d96478da07107ca59',client_secret='5a034c056898492b966e34c01a995d28',redirect_uri='https://oauth.pstmn.io/v1/browser-callback')
 
-    # SETTINGS
-    # refreshing token
-    token = util.prompt_for_user_token("toni1li131", scope)
-    #print(token)
-    #user_id = "toni1li131"
+  # SETTINGS
+  # refreshing token
+  token = util.prompt_for_user_token("toni1li131", scope)
+  #print(token)
+  #user_id = "toni1li131"
 
-    # ------------------ getting list of playlists from user ---------------------- #
-    # PERFORM THE QUERY
-    query = "https://api.spotify.com/v1/users/" + user_id + "/playlists"
-    # print(query)
+  # ------------------ getting list of playlists from user ---------------------- #
+  # PERFORM THE QUERY
+  query = "https://api.spotify.com/v1/users/" + user_id + "/playlists"
+  # print(query)
 
-    response = requests.get(query,
-                            headers={"Content-Type": "application/json",
-                                        "Authorization": f"Bearer {token}"})
-    json_response = response.json()
+  response = requests.get(query,
+                          headers={"Content-Type": "application/json",
+                                      "Authorization": f"Bearer {token}"})
+  json_response = response.json()
 
-    playlists = []
-    for i, j in enumerate(json_response['items']):
-        #print(f"{i + 1}) \"{j['name']}\" at {j['href']}")
-        playlists.append(j['href'])
-
-
-    # print('Public Playlists:')
-    # print(playlists)
-
-    #test = ["https://api.spotify.com/v1/playlists/1Am6znV9CWkrdUwpAAPpK6"]
-    # ------------------ getting list of songs in the playlist(s) ---------------------- #
-    playlistURIs = []
-    for i in playlists:
-        # PERFORM THE QUERY
-        query = i + "/tracks?offset=0&market=US"
-        #print(query)
-
-        response = requests.get(query,
-                            headers={"Content-Type": "application/json",
-                                        "Authorization": f"Bearer {token}"})
-        json_response = response.json()
-        #print(json_response)
-        for i, j in enumerate(json_response['items']):
-            #print(f"{i + 1}) \"{j['track']['uri']}")
-            playlistURIs.append(j['track']['uri'])
-
-    #print(playlistURIs)
+  playlists = []
+  for i, j in enumerate(json_response['items']):
+      #print(f"{i + 1}) \"{j['name']}\" at {j['href']}")
+      playlists.append(j['href'])
 
 
-    # ------------------ getting track information for song ---------------------- #
-    song = song.split("/")
-    songID = song[4]
-    songID = songID.split("?")
-    songID = songID[0]
-    
-    #print(songID)
+  # print('Public Playlists:')
+  # print(playlists)
 
-    # PERFORM THE QUERY
-    query = "https://api.spotify.com/v1/tracks/" + songID + "?market=US"
+  #test = ["https://api.spotify.com/v1/playlists/1Am6znV9CWkrdUwpAAPpK6"]
+  # ------------------ getting list of songs in the playlist(s) ---------------------- #
+  playlistURIs = []
+  for i in playlists:
+      # PERFORM THE QUERY
+      query = i + "/tracks?offset=0&market=US"
+      #print(query)
 
-    response = requests.get(query,
-                        headers={"Content-Type": "application/json",
-                                    "Authorization": f"Bearer {token}"})
-    json_response = response.json()
-    #print(json_response['uri'])
+      response = requests.get(query,
+                          headers={"Content-Type": "application/json",
+                                      "Authorization": f"Bearer {token}"})
+      json_response = response.json()
+      #print(json_response)
+      for i, j in enumerate(json_response['items']):
+          #print(f"{i + 1}) \"{j['track']['uri']}")
+          playlistURIs.append(j['track']['uri'])
 
-    songURI = json_response['uri']
+  #print(playlistURIs)
 
-    # ------------------ checking to see if song URI is in any of the playlists ---------------------- #
-    if songURI in playlistURIs:
-        message = "The song was found in their library, better not recommend it :("
-    else:
-        message = "Recommend the song!"
 
-    return(message)
+  # ------------------ getting track information for song ---------------------- #
+  song = song.split("/")
+  songID = song[4]
+  songID = songID.split("?")
+  songID = songID[0]
+  
+  #print(songID)
+
+  # PERFORM THE QUERY
+  query = "https://api.spotify.com/v1/tracks/" + songID + "?market=US"
+
+  response = requests.get(query,
+                      headers={"Content-Type": "application/json",
+                                  "Authorization": f"Bearer {token}"})
+  json_response = response.json()
+  #print(json_response['uri'])
+
+  songURI = json_response['uri']
+
+  # ------------------ checking to see if song URI is in any of the playlists ---------------------- #
+  if songURI in playlistURIs:
+      message = "The song was found in their library, better not recommend it :("
+  else:
+      message = "Recommend the song!"
+
+  return(message)
 
 @app.route("/")
 def home():
