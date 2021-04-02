b9be4b29547e8d56eb647d554c10e9afbabb6e8f
toni-li <toni.li@uwaterloo.ca>
2021-04-01 22:05:18 -0400

    trying to refresh token again

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ def auth():
 
 @app.route('/<path:text>', methods=['GET', 'POST'])
 def all_routes(text):
-    if text.startswith('callback'):
+    if text.startswith('https://spotify-recos.herokuapp.com/callback'):
         auth_token = request.args['code']
         code_payload = {
             "grant_type": "authorization_code",
