0c7eed0defb17b3579cd27306bab197664bfe09d
toni-li <toni.li@uwaterloo.ca>
2021-04-03 14:48:38 -0400

    adding callback

--- app/main.py
+++ app/main.py
@@ -1,47 +1,54 @@
 import os
-from flask import Flask, render_template, redirect, g, request
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 import json
+from flask import Flask, request, redirect, g, render_template
 import requests
 from urllib.parse import quote
 
+# Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
+# Visit this url to see all the steps, parameters, and expected response.
 
-AUTHORIZATION_HEADER = ""
+
+app = Flask(__name__)
+
 #  Client Keys
 CLIENT_ID = "4300c682d48b480d96478da07107ca59"
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 
 # Server-side Parameters
+CLIENT_SIDE_URL = "https://spotify-recos.herokuapp.com"
+REDIRECT_URI = "{}/callback/q".format(CLIENT_SIDE_URL)
+SCOPE = "playlist-modify-public playlist-modify-private"
+STATE = ""
 SHOW_DIALOG_bool = True
 SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
-
-REDIRECT_URI = "https://spotify-recos.herokuapp.com/callback/q" 
-#REDIRECT_URI = "http://127.0.0.1:5000/callback/q"
 
 auth_query_parameters = {
     "response_type": "code",
     "redirect_uri": REDIRECT_URI,
-    "scope": "playlist-modify-private user-top-read",
+    "scope": SCOPE,
+    # "state": STATE,
+    # "show_dialog": SHOW_DIALOG_str,
     "client_id": CLIENT_ID
 }
 
-app = Flask(__name__)
-
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-@app.route("/login", methods=["POST"])
-def auth():
-
+    # Auth Step 1: Authorization
     url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
-    auth_url = "{}/?{}".format("https://accounts.spotify.com/authorize", url_args)
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
     return redirect(auth_url)
 
-@app.route("/callback/q", methods=['GET','POST'])
+
+@app.route("/callback/q")
 def callback():
+    # Auth Step 4: Requests refresh and access tokens
     auth_token = request.args['code']
     code_payload = {
         "grant_type": "authorization_code",
@@ -50,20 +57,19 @@ def callback():
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
     }
-    post_request = requests.post("https://accounts.spotify.com/api/token", data=code_payload)
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
 
+    # Auth Step 5: Tokens are Returned to Application
     response_data = json.loads(post_request.text)
     access_token = response_data["access_token"]
     refresh_token = response_data["refresh_token"]
     token_type = response_data["token_type"]
     expires_in = response_data["expires_in"]
 
-    global AUTHORIZATION_HEADER    # Needed to modify global copy of globvar
-    AUTHORIZATION_HEADER  = {"Authorization": "Bearer {}".format(access_token)}
+    # Auth Step 6: Use the access token to access Spotify API
+    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
 
     return render_template("web-app.html")
 
-    
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
-
+    app.run(debug=True, port=PORT)
